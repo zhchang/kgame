3ec04f7d2019131cec81d0ee4e8a759b4a95fa90
zhchang <zhchang@gmail.com>
2014-10-27 12:30:10 +0800

    0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from disutils.core import setup
 setup(
         name='kgame',
         packages=['kgame'],
-        version = '0.1',
+        version = '0.2',
         description = 'a kivy game engine',
         author = 'Zhao Chang',
         author_email='zhchang@gmail.com',
         url='https://github.com/zhchang/kgame'
-        download_url='https://github.com/zhchang/kgame/tarball/0.1',
+        download_url='https://github.com/zhchang/kgame/tarball/0.22',
         keywords = ['kivy','game engine'],
         classifiers = [],
 )
