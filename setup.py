7f86a31c9b7daafb1f3b4b540f77c7208802e6a2
zhchang <zhchang@gmail.com>
2014-11-04 11:04:03 +0800

    0.92

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
         name='kgame',
         packages=['kgame'],
-        version = '0.91',
+        version = '0.92',
         description = 'a kivy game engine',
         author = 'Zhao Chang',
         author_email='zhchang@gmail.com',
